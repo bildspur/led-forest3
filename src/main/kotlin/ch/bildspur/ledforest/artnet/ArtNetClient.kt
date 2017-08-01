b12eef8661e36b7eea5c4cafd22ed10261977083
Florian Bruggisser <florian@nexpose.ch>
2017-08-01 11:49:17 +0200

    added relevant libraries and updated processing core libs

---
+++ src/main/kotlin/ch/bildspur/ledforest/artnet/ArtNetClient.kt
@@ -0,0 +1,72 @@
+package ch.bildspur.ledforest.artnet
+
+import artnet4j.ArtNet
+import artnet4j.ArtNetException
+import artnet4j.ArtNetNode
+import artnet4j.packets.ArtDmxPacket
+import java.net.InetAddress
+import java.net.SocketException
+
+class ArtNetClient {
+    private var sequenceId: Int = 0
+    private var artnet = ArtNet()
+    private var receiver: ArtNetNode? = null
+
+    fun open() {
+        open(null)
+    }
+
+    fun open(address: String?) {
+        try {
+            artnet.start()
+            setReceiver(address)
+        } catch (e: SocketException) {
+            e.printStackTrace()
+        } catch (e: ArtNetException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    fun setReceiver(address: String?) {
+        if (null == address)
+            receiver = null
+
+        receiver = createNode(address!!)
+    }
+
+    fun createNode(address: String): ArtNetNode? {
+        try {
+            val node = ArtNetNode()
+            node.ipAddress = InetAddress.getByName(address)
+            return node
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return null
+    }
+
+    fun close() {
+        artnet.stop()
+    }
+
+    fun send(universe: Int, data: ByteArray) {
+        send(receiver, universe, data)
+    }
+
+    fun send(node: ArtNetNode?, universe: Int, data: ByteArray) {
+        val dmx = ArtDmxPacket()
+
+        dmx.setUniverse(0, universe)
+        dmx.sequenceID = sequenceId % 256
+        dmx.setDMX(data, data.size)
+
+        if (receiver != null)
+            artnet.unicastPacket(dmx, node)
+        else
+            artnet.broadcastPacket(dmx)
+
+        sequenceId++
+    }
+}
