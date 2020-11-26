4e069c4eed8fd3ab059369835f3f36b60af856d9
Florian Bruggisser <cansik@users.noreply.github.com>
2020-11-26 01:28:47 +0100

    updated easing methods

--- src/main/kotlin/ch/bildspur/ledforest/util/Easing.kt
+++ src/main/kotlin/ch/bildspur/ledforest/util/Easing.kt
@@ -6,28 +6,73 @@ import processing.core.PConstants.PI
  * Created by cansik on 08.06.17.
  */
 object Easing {
-    // t = current time, b = start value, c = change in value, d = duration
-    fun easeInQuad(time: Float, b: Float, c: Float, d: Float): Float {
+    // no easing} no acceleration
+    fun linear(t: Float): Float {
+        return t
+    }
+
+    // accelerating from zero velocity
+    fun easeInQuad(t: Float): Float {
+        return t * t
+    }
+
+    // decelerating to zero velocity
+    fun easeOutQuad(t: Float): Float {
+        return t * (2 - t)
+    }
+
+    // acceleration until halfway} then deceleration
+    fun easeInOutQuad(t: Float): Float {
+        return if (t < .5) 2 * t * t else -1 + (4 - 2 * t) * t
+    }
+
+    // accelerating from zero velocity
+    fun easeInCubic(t: Float): Float {
+        return t * t * t
+    }
+
+    // decelerating to zero velocity
+    fun easeOutCubic(time: Float): Float {
         var t = time
-        t /= d
-        return c * t * t + b
+        return (--t) * t * t + 1
     }
 
-    fun linearTween(t: Float, b: Float, c: Float, d: Float): Float {
-        return c * t / d + b
+    // acceleration until halfway} then deceleration
+    fun easeInOutCubic(t: Float): Float {
+        return if (t < .5) 4 * t * t * t else (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
     }
 
-    fun easeOutQuad(time: Float, b: Float, c: Float, d: Float): Float {
+    // accelerating from zero velocity
+    fun easeInQuart(t: Float): Float {
+        return t * t * t * t
+    }
+
+    // decelerating to zero velocity
+    fun easeOutQuart(time: Float): Float {
         var t = time
-        t /= d
-        return -c * t * (t - 2) + b
+        return 1 - (--t) * t * t * t
     }
 
-    fun easeInSine(t: Float, b: Float, c: Float, d: Float): Float {
-        return Math.round(-c * Math.cos(t / d * (PI / 2.0)) + c.toDouble() + b.toDouble()).toFloat()
+    // acceleration until halfway} then deceleration
+    fun easeInOutQuart(time: Float): Float {
+        var t = time
+        return if (t < .5) 8 * t * t * t * t else 1 - 8 * (--t) * t * t * t
     }
 
-    fun easeOutSine(t: Float, b: Float, c: Float, d: Float): Float {
-        return Math.round(c * Math.sin(t / d * (PI / 2.0)) + b).toFloat()
+    // accelerating from zero velocity
+    fun easeInQuint(t: Float): Float {
+        return t * t * t * t * t
+    }
+
+    // decelerating to zero velocity
+    fun easeOutQuint(time: Float): Float {
+        var t = time
+        return 1 + (--t) * t * t * t * t
+    }
+
+    // acceleration until halfway} then deceleration
+    fun easeInOutQuint(time: Float): Float {
+        var t = time
+        return if (t < .5) 16 * t * t * t * t * t else 1 + 16 * (--t) * t * t * t * t
     }
 }
