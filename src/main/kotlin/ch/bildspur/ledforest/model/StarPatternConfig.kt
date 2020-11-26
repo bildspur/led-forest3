6d0a3e3628edfff2a5dbe49f08bb6007aae8436a
Florian Bruggisser <florian@bildspur.ch>
2020-11-26 13:35:21 +0100

    renamed parameter

--- src/main/kotlin/ch/bildspur/ledforest/model/StarPatternConfig.kt
+++ src/main/kotlin/ch/bildspur/ledforest/model/StarPatternConfig.kt
@@ -22,7 +22,7 @@ class StarPatternConfig {
     var fadeSpeed = DataModel(0.01f)
 
     @Expose
-    @SliderParameter("Timer Speed", 0.0, 1000.0, 10.0, snap = true)
+    @SliderParameter("Timer Interval", 0.0, 1000.0, 10.0, snap = true)
     var timerSpeed = DataModel(500L)
 
     @Expose
