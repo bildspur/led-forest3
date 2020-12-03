4cb4eb84affd453788a8d65f8ef90faa55657f67
Florian Bruggisser <florian@bildspur.ch>
2020-12-03 21:44:09 +0100

    removed multi-loop for object removal

--- src/main/kotlin/ch/bildspur/tracking/simple/SimpleTracker.kt
+++ src/main/kotlin/ch/bildspur/tracking/simple/SimpleTracker.kt
@@ -2,16 +2,15 @@ package ch.bildspur.tracking.simple
 
 import ch.bildspur.math.Float2
 import ch.bildspur.math.distance
-import ch.bildspur.tracking.TrackedEntity
 import ch.bildspur.tracking.Tracker
 import java.util.concurrent.atomic.AtomicInteger
 
 class SimpleTracker<TItem>(inline val position: (item: TItem) -> Float2,
-                       var maxDelta: Float,
-                       var maxUntrackedTime : Long = 100,
-                       inline val onAdd: (entity : TrackedEntity<TItem>) -> Unit = {},
-                       inline val onUpdate: (entity : TrackedEntity<TItem>, item: TItem) -> Unit = { e, i -> e.item = i },
-                       inline val onRemove: (entity : TrackedEntity<TItem>) -> Unit = {}) : Tracker<SimpleTrackedEntity<TItem>, TItem> {
+                           var maxDelta: Float,
+                           var maxUntrackedTime: Long = 100,
+                           inline val onAdd: (entity: SimpleTrackedEntity<TItem>) -> Unit = {},
+                           inline val onUpdate: (entity: SimpleTrackedEntity<TItem>, item: TItem) -> Unit = { e, i -> e.item = i },
+                           inline val onRemove: (entity: SimpleTrackedEntity<TItem>) -> Unit = {}) : Tracker<SimpleTrackedEntity<TItem>, TItem> {
     private val trackingIdCounter = AtomicInteger(0)
     override val entities = mutableListOf<SimpleTrackedEntity<TItem>>()
 
@@ -21,23 +20,24 @@ class SimpleTracker<TItem>(inline val position: (item: TItem) -> Float2,
         entities.forEach { it.matched = false }
 
         detectedEntities.matchNearest(entities,
-            maxDelta.toDouble(),
-            distance = { s, d -> position(s.item).distance(d.position).toDouble() },
-            matched = { it.matched },
-            onMatch = { s, d ->
-                d.matched = true
-                s.matched = true
-                d.position = position(s.item)
-                d.lastMatchTimeStamp = millis
-                onUpdate(d, s.item)
-            }
+                maxDelta.toDouble(),
+                distance = { s, d -> position(s.item).distance(d.position).toDouble() },
+                matched = { it.matched },
+                onMatch = { s, d ->
+                    d.matched = true
+                    s.matched = true
+                    d.position = position(s.item)
+                    d.lastMatchTimeStamp = millis
+                    onUpdate(d, s.item)
+                }
         )
 
         // clean up entities
-        // todo: optimize this step to use less loops
-        val entitiesToRemove = entities.filter { !it.matched && it.getLifeTime(millis) > maxUntrackedTime }
-        entitiesToRemove.forEach { onRemove(it) }
-        entities.removeAll(entitiesToRemove)
+        entities.removeAll(entities.filter { !it.matched && it.getLifeTime(millis) > maxUntrackedTime }
+                .map {
+                    onRemove(it)
+                    it
+                })
 
         // add new entities
         detectedEntities.filter { !it.matched }.forEach {
