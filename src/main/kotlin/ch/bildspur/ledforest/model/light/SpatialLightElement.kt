5bde30cf62211f07b64d980d30b224ad797d7b5a
Florian Bruggisser <florian@bildspur.ch>
2022-05-11 19:43:21 +0200

    implemented spatial light elemnt

---
+++ src/main/kotlin/ch/bildspur/ledforest/model/light/SpatialLightElement.kt
@@ -0,0 +1,43 @@
+package ch.bildspur.ledforest.model.light
+
+import ch.bildspur.ledforest.configuration.PostProcessable
+import ch.bildspur.ledforest.ui.properties.PVectorAngleParameter
+import ch.bildspur.ledforest.ui.properties.PVectorParameter
+import ch.bildspur.ledforest.util.SpaceInformation
+import ch.bildspur.model.DataModel
+import com.google.gson.annotations.Expose
+import processing.core.PVector
+
+abstract class SpatialLightElement(
+        universe: DataModel<Int> = DataModel(0),
+        addressStart: DataModel<Int> = DataModel(0),
+        @PVectorParameter("Position") @Expose val position: DataModel<PVector> = DataModel(PVector()),
+        @PVectorAngleParameter("Rotation") @Expose val rotation: DataModel<PVector> = DataModel(PVector()),
+        initialLEDCount: Int = 1
+) : LightElement(universe, addressStart, initialLEDCount), PostProcessable {
+
+
+    init {
+        hookPositionListener()
+    }
+
+    private fun hookPositionListener() {
+        position.onChanged += {
+            recalculateLEDPosition()
+        }
+        rotation.onChanged += {
+            recalculateLEDPosition()
+        }
+    }
+
+    fun recalculateLEDPosition() {
+        leds.forEachIndexed { index, led ->
+            led.position = ledPositionByIndex(index)
+        }
+    }
+
+    override fun gsonPostProcess() {
+        super.gsonPostProcess()
+        hookPositionListener()
+    }
+}
--- src/main/kotlin/ch/bildspur/ledforest/model/light/Tube.kt
+++ src/main/kotlin/ch/bildspur/ledforest/model/light/Tube.kt
@@ -1,20 +1,18 @@
 package ch.bildspur.ledforest.model.light
 
-import ch.bildspur.ledforest.configuration.PostProcessable
-import ch.bildspur.ledforest.ui.properties.*
-import ch.bildspur.ledforest.util.ColorMode
 import ch.bildspur.ledforest.util.SpaceInformation
 import ch.bildspur.model.DataModel
-import ch.bildspur.ui.properties.*
+import ch.bildspur.ui.properties.EnumParameter
+import ch.bildspur.ui.properties.NumberParameter
 import com.google.gson.annotations.Expose
 import processing.core.PVector
 
 
 class Tube(universe: DataModel<Int> = DataModel(0),
            addressStart: DataModel<Int> = DataModel(0),
-           @PVectorParameter("Position") @Expose val position: DataModel<PVector> = DataModel(PVector()),
-           @PVectorAngleParameter("Rotation") @Expose val rotation: DataModel<PVector> = DataModel(PVector()))
-    : LightElement(universe, addressStart, initialLEDCount = 24), PostProcessable {
+           position: DataModel<PVector> = DataModel(PVector()),
+           rotation: DataModel<PVector> = DataModel(PVector()))
+    : SpatialLightElement(universe, addressStart, position, rotation, initialLEDCount = 24) {
 
     var isSelected = DataModel(false)
 
@@ -30,19 +28,6 @@ class Tube(universe: DataModel<Int> = DataModel(0),
     @EnumParameter("Tag")
     var tag = DataModel(TubeTag.Interaction)
 
-    init {
-        hookPositionListener()
-    }
-
-    private fun hookPositionListener() {
-        position.onChanged += {
-            recalculateLEDPosition()
-        }
-        rotation.onChanged += {
-            recalculateLEDPosition()
-        }
-    }
-
     override fun ledPositionByIndex(index: Int): PVector {
         // this can happen because light element is initialized first
         if (length == null) {
@@ -52,22 +37,6 @@ class Tube(universe: DataModel<Int> = DataModel(0),
         return SpaceInformation.calculateLEDPosition(index, this)
     }
 
-    fun recalculateLEDPosition() {
-        leds.forEachIndexed { index, led ->
-            led.position = SpaceInformation.calculateLEDPosition(index, this)
-        }
-    }
-
     val ledLength: Float
         get() = length.value / ledCount.value
-
-    override fun toString(): String {
-        return "${name.value} ${universe.value + 1}.${startAddress + 1}-${endAddress + 1} (${ledCount.value})"
-    }
-
-    override fun gsonPostProcess() {
-        super.gsonPostProcess()
-        hookPositionListener()
-        recalculateLEDPosition()
-    }
 }
